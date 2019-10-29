f550b940df539ad10360238dcba558c5ffb1010b
Orlando Zuniga <zunigao@eduroam-173-148.dyn.carleton.edu>
2019-10-29 09:54:12 -0500

    added encoder

--- model.py
+++ model.py
@@ -1,4 +1,30 @@
 import torch.nn as nn
+class GeneratorEncoder(nn.Module):
+    def __init__(self, use_vgg=True):
+        super(Generator, self).__init__()
+            self.encoder = nn.Sequential(
+                nn.Conv2d(3, 64, 3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(64, 128, 4, 2, padding=1, bias=False),
+                nn.BatchNorm2d(128),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(128, 256, 4, 2, padding=1, bias=False),
+                nn.BatchNorm2d(256),
+                nn.ReLU(inplace=True)
+                nn.Conv2d(256, 512, 4, 2, padding=1, bias=False),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True)
+            )
+    def forward(self, img, txt_feat):
+        img_feat = self.encoder(img)
+        z_mean = self.mu(txt_feat)
+        z_log_stddev = self.log_sigma(txt_feat)
+        z = torch.randn(txt_feat.size(0), 128)
+        #if next(self.parameters()).is_cuda:
+         #   z = z.cuda()
+        txt_feat = z_mean + z_log_stddev.exp() * Variable(z)
+        return img_feat, text_feat
+
 
 class ResidualBlock(nn.Module):
     def __init__(self):
